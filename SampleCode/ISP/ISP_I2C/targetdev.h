3c9537f40b41cdc503d9a3b0d022c0cc46b01726
mjchen <mjchen@nuvoton.com>
2019-10-31 09:52:43 +0800

    add ISP I2C/RS485 sample
    Change-Id: I1b53658f6162e5b41cde531d71a3bde9672ab9fd

---
+++ SampleCode/ISP/ISP_I2C/targetdev.h
@@ -0,0 +1,15 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "Nano100Series.h"
+#include "i2c_transfer.h"
+#include "ISP_USER.h"
+
+#define I2C_ADDR 0x60
+
