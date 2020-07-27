363d1788125e86305146b65317b54d46fc324017
MS10 CFLi0 <CFLi0@nuvoton.com>
2020-07-27 15:22:02 +0800

    Correct USB device descriptor bcdUSB field
    Change-Id: Ie81a8b71882e0ce1cd4425bf17f0eacb3b28b3df

--- SampleCode/StdDriver/USBD_VCOM_And_Mass_Storage/descriptors.c
+++ SampleCode/StdDriver/USBD_VCOM_And_Mass_Storage/descriptors.c
@@ -18,7 +18,7 @@ uint8_t gu8DeviceDescriptor[] =
 {
     LEN_DEVICE,         /* bLength */
     DESC_DEVICE,        /* bDescriptorType */
-    0x00, 0x02,         /* bcdUSB */
+    0x10, 0x01,         /* bcdUSB */
     0xEF,               /* bDeviceClass: IAD*/
     0x02,               /* bDeviceSubClass */
     0x01,               /* bDeviceProtocol */
@@ -26,14 +26,14 @@ uint8_t gu8DeviceDescriptor[] =
     /* idVendor */
     USBD_VID & 0x00FF,
     (USBD_VID & 0xFF00) >> 8,
-                        /* idProduct */
-                        USBD_PID & 0x00FF,
-                        (USBD_PID & 0xFF00) >> 8,
-                        0x00, 0x01,        /* bcdDevice */
-                        0x01,              /* iManufacture */
-                        0x02,              /* iProduct */
-                        0x03,              /* iSerialNumber - no serial */
-                        0x01               /* bNumConfigurations */
+    /* idProduct */
+    USBD_PID & 0x00FF,
+    (USBD_PID & 0xFF00) >> 8,
+    0x00, 0x01,        /* bcdDevice */
+    0x01,              /* iManufacture */
+    0x02,              /* iProduct */
+    0x03,              /* iSerialNumber - no serial */
+    0x01               /* bNumConfigurations */
 };
 
 /*!<USB Configure Descriptor */
